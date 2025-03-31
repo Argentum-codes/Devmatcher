6f7a7e98cf25dc5e32f5c020e631892c42f81bff
Argha Guha <arghaguha3001@gmail.com>
2025-03-31 20:57:32 +0530

    exploring get, post, delte

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ app.use("/test" , (req, res) => {
     res.send("Hello from the server")
 })
 
+app.get("/user", (req, res) => {
+    res.send({name: "John", age: 30})
+})
+
+app.post("/user", (req, res) => {
+    res.send("Save data to the db")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("User Deleted Successfully")
+})
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
